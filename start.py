db5214250ad92864cbb5b7ac787ba02e0e49400b
0312birdzhang <0312birdzhang@gmail.com>
2015-11-30 10:52:32 +0800

    添加默认vol号

--- start.py
+++ start.py
@@ -6,6 +6,7 @@ Created on 2015年10月14日
 @author: 0312birdzhang
 '''
 import web
+import time
 from getones import *
 import sys,os 
 reload(sys)
@@ -21,9 +22,17 @@ class index:
 class query:
     def GET(self):
         web.header('Content-Type', 'application/json')
-        i = web.input()
+        i = web.input(vol=getTodayVol())
         return getTodayContent(i.vol)
-    
+ 
+def getTodayVol():
+       #new Date("2012-10-07 00:00:00").getTime();
+       start = "2012-10-07 00:00:00"
+       timeArray = time.strptime(start, "%Y-%m-%d %H:%M:%S")
+       timeStamp = int(time.mktime(timeArray))
+       today = int(time.time())
+       vol = (today - timeStamp)/60/60/24
+       return vol
 if __name__ == "__main__":
     app = web.application(urls, globals())
     app.run()
