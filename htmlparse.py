a9624fac794906c4c7c731f090ef77f4440a1a47
BirdZhang <0312birdzhang@gmail.com>
2016-03-25 23:02:29 +0800

    update parset py

--- htmlparse.py
+++ htmlparse.py
@@ -5,27 +5,44 @@ import sys
 import urllib2
 import datetime
 import json
+import time
+import datetime
 
-def getHtml(date,type):
+url = 'http://v3.wufazhuce.com:8000/api/'
+
+def getHtml(api):
     i_headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36",\
-                 "Referer": 'http://www.baidu.com'}
-    req = urllib2.Request('http://m.wufazhuce.com/'+type+'/'+date, headers=i_headers)
+                 "Referer": 'http://www.wufazhuce.com/'}
+    req = urllib2.Request(url+api, headers=i_headers)
     try:
         response = urllib2.urlopen(req,timeout=30)
         allhtml = response.read()
         return allhtml
     except Exception as e:
         print(e)
-        
-def queryContent(date):
-    allhtml = getHtml(date,"one")
-    soup = BeautifulSoup(allhtml,"html.parser")
-    titulo = soup.find_all("span", "picture-detail-issue-no")[0].contents[0].strip() #VOL号
-    imagen = soup.find_all("img", "item-picture-img")[0]["src"].strip() #首页图片链接
-    imagen_leyenda =soup.find_all("p", "text-author")[0].contents[1].strip() #图片标题
-    
-    cita = soup.find_all("p", "text-content")[0].contents[0].strip() #首页内容
 
+def gettimedelta(date):
+    today = time.strftime("%Y-%m-%d", time.localtime())
+    today = datetime.datetime.strptime(today,'%Y-%m-%d')
+    thatday = datetime.datetime.strptime(date,'%Y-%m-%d')
+    timedelta = (today - thatday).days
+    return timedelta
+    
+    
+def queryContent(date):
+    
+    vindex = gettimedelta(date)
+    #home page
+    h_api = "hp/idlist/0"
+    h_datas = getHtml(h_api)
+    h_index = h_datas["data"][vindex]
+    h_detail_api = "hp/detail/"+h_index
+    h_detail_datas = getHtml(h_detail_api)
+    h_data = h_detail_datas.get("data")
+    titulo = h_data.get("hp_title")
+    imagen = h_data.get("hp_img_url")
+    imagen_leyenda =h_data.get("hp_author")
+    cita = h_data.get("hp_content")
     cita_content=""
     cita_author=""
     if "by" in cita:
@@ -34,66 +51,56 @@ def queryContent(date):
     else:
         cita_content = cita.split("from")[0]
         cita_author = "from "+cita.split("from")[1]
-    dom = soup.find_all("p", "day")[0].contents[0].strip() #day
-    may = soup.find_all("p", "month")[0].contents[0].strip() #month
-    
-    #获取文章
-    allhtml = getHtml(date,"article")
-    soup = BeautifulSoup(allhtml,"html.parser")
-    comilla_cerrar = soup.find_all("p","text-lead")[0].contents[0].strip() #文章最上面的文字
-    articulo_titulo = soup.find_all("p","text-title")[0].contents[0].strip() #文章标题
-    articulo_autor = soup.find_all("p","text-author")[0].contents[0].strip() #文章作者
-    articulo_contenido = soup.find_all("div","text-content")[0].stripped_strings #文章内容
-    articulo_contenido ="".join(unicode(item) for item in  articulo_contenido)
-    articulo_tmp = soup.find_all("p","text-editor")
-    articulo_editor = articulo_tmp[0].contents[0].strip() if len(articulo_tmp) > 0 else "" #责任编辑
+    makettime = h_data.get("hp_makettime")
+    dom = datetime.datetime.strptime(makettime, '%Y-%m-%d %H:%M:%S').date().strftime("%d")
+    may =  datetime.datetime.strptime(makettime, '%Y-%m-%d %H:%M:%S').date().strftime("%B %Y")
 
-    #获取问题
-    allhtml = getHtml(date,"question")
-    soup = BeautifulSoup(allhtml,"html.parser")
-    cuestion_title = soup.find_all("p","text-title")[0].contents[0].strip() #问题-标题
-    cuestion_question = soup.find_all("div","text-content")[0].stripped_strings#问题-问
-    cuestion_question = ''.join(unicode(item) for item in  cuestion_question)
-    cuestion_answerer = soup.find_all("p","text-title")[1].stripped_strings #问题-回答者
-    cuestion_answerer = ''.join(unicode(item) for item in  cuestion_answerer)
-    cuestion_contenians = soup.find_all("div","text-content")[1].stripped_strings #问题-答
-    cuestion_contenians = ''.join(unicode(item) for item in  cuestion_contenians)
-    
-    #东西已经过期
-#     cosas_imagen_tmp = soup.find_all("div", "cosas-imagen")[0].find("img")
-#     cosas_imagen = cosas_imagen_tmp['src'].strip() if cosas_imagen_tmp.get("src") else 'http://www.example.com' #东西里面的图片链接
-#     cosas_titulo = ''.join(unicode(item) for item in  soup.find_all("h2","cosas-titulo")[0].stripped_strings) #东西名
-#     cosas_contenido = soup.find_all("div","cosas-contenido")[0].stripped_strings #东西说明
-#     cosas_contenido = ''.join(unicode(item) for item in  cosas_contenido)
+    #######other
+    o_api = "reading/index/"
+    o_datas = getHtml(o_api)
+    essay_index = o_datas.get("data").get("essay")[vindex].get("content_id")
+    question_index = o_datas.get("data").get("question")[vindex].get("question_id")
+    essay_api = "essay/" + essay_index
+    essay_datas = getHtml(essay_api).get("data")
+    comilla_cerrar = essay_datas.get("guide_word")
+    articulo_titulo = essay_datas.get("hp_title")
+    articulo_autor = essay_datas.get("hp_author")
+    articulo_contenido = essay_datas.get("hp_content")
+    articulo_editor = essay_datas.get("hp_author_introduce")
+    question_api = "question/"+question_index
+    question_datas = getHtml(question_api).get("data")
+    cuestion_title = question_datas.get("question_title")
+    cuestion_question = question_datas.get("question_content")
+    cuestion_answerer = question_datas.get("answer_title")
+    cuestion_contenians = question_datas.get("answer_content")
     cosas_imagen = ""
     cosas_titulo = ""
     cosas_contenido = ""
-    
     one_map= {
-        "titulo":titulo,
-        "imagen":imagen,
-        "imagen_leyenda":imagen_leyenda,
-        "cita_content":cita_content,
-        "cita_author":cita_author,
-        "dom":dom,
-        "may":may,
+            "titulo":titulo,
+            "imagen":imagen,
+            "imagen_leyenda":imagen_leyenda,
+            "cita_content":cita_content,
+            "cita_author":cita_author,
+            "dom":dom,
+            "may":may,
 
-        "comilla_cerrar":comilla_cerrar,
-        "articulo_titulo":articulo_titulo,
-        "articulo_autor":articulo_autor,
-        "articulo_contenido":articulo_contenido,
-        "articulo_editor":articulo_editor,
+            "comilla_cerrar":comilla_cerrar,
+            "articulo_titulo":articulo_titulo,
+            "articulo_autor":articulo_autor,
+            "articulo_contenido":articulo_contenido,
+            "articulo_editor":articulo_editor,
 
-        "cuestion_title":cuestion_title,
-        "cuestion_question":cuestion_question,
-        "cuestion_answerer":cuestion_answerer,
-        "cuestion_contenians":cuestion_contenians,
+            "cuestion_title":cuestion_title,
+            "cuestion_question":cuestion_question,
+            "cuestion_answerer":cuestion_answerer,
+            "cuestion_contenians":cuestion_contenians,
 
-        "cosas_imagen":cosas_imagen,
-        "cosas_titulo":cosas_titulo,
-        "cosas_contenido":cosas_contenido
-    }
-    print one_map
+            "cosas_imagen":cosas_imagen,
+            "cosas_titulo":cosas_titulo,
+            "cosas_contenido":cosas_contenido
+        }
+    #print(one_map)
     return one_map
 
     
